068dad8e193d6a081245883979a000ad909bd8d7
everdolphin1989 <32991886+everdolphin1989@users.noreply.github.com>
2018-04-23 15:51:14 -0500

    Add files via upload

---
+++ HWpackage/R/AlphabetSample.R
@@ -0,0 +1,6 @@
+# Function to return a letter from alphabet
+
+generator.ftn <- function() {
+  x <- sample(c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'),1)
+  return(x)
+}
